9a47cf23c3b468ffd600f691a57a6c99a99195fa
Abhishek Kumawat <65283365+abhishekkmwt@users.noreply.github.com>
2023-06-18 22:35:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // complete the given function
 
 function palindrome(str){
+	let s= str.toLowerCase();
 	let start=0;
 	let end =str.length-1;
 	while(i<j){
-		if(str[i] !== str[j]){
+		if(s[i] !== s[j]){
 			return false;
 		}
 	}
