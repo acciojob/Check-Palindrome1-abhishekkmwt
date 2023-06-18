05dbcfe301effc068794f866825a9c52695ee83d
Abhishek Kumawat <65283365+abhishekkmwt@users.noreply.github.com>
2023-06-18 22:34:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
+	let start=0;
+	let end =str.length-1;
+	while(i<j){
+		if(str[i] !== str[j]){
+			return false;
+		}
+	}
+	return true;
 
 }
 module.exports = palindrome
